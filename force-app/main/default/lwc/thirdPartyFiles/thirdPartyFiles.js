4c85344e5833baa6088fb32d3d63f85f119137b8
Bollapu Venkata Rama Subba R <subbareddy.bollapu@wisseninfotech.com>
2022-10-27 15:25:58 +0530

    add some files

---
+++ force-app/main/default/lwc/thirdPartyFiles/thirdPartyFiles.js
@@ -0,0 +1,27 @@
+import { LightningElement } from 'lwc';
+import MOMENT from '@salesforce/resourceUrl/moment'
+//to lead the file we need to do another import
+import {loadScript} from 'lightning/platformResourceLoader'
+export default class ThirdPartyFiles extends LightningElement {
+    currentDate=''
+    isLibLoaded = false
+    renderedCallback(){
+        if(this.isLibLoaded){
+            return
+        } else{
+            Promise.all([
+                loadScript(this, MOMENT+'/moment/moment.min.js')
+            ]).then(()=>{
+                //success
+                this.setDateOnScreen()
+            }).catch(error=>{
+                console.error(error)
+            })
+            this.isLibLoaded = true
+        }
+        
+    }
+    setDateOnScreen(){
+        this.currentDate = moment().format('LLLL')
+    }
+}
